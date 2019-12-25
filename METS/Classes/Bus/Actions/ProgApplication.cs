298e45b40ca04cd33eb355eb0cc155984e0da65f
thewhobox <admin@mikegerst.de>
2019-12-25 18:47:52 +0100

    Fixed display bug in ProgApplication

--- METS/Classes/Bus/Actions/ProgApplication.cs
+++ METS/Classes/Bus/Actions/ProgApplication.cs
@@ -6,10 +6,12 @@ using METS.Knx.Builders;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Windows.Storage;
 
 namespace METS.Classes.Bus.Actions
@@ -24,6 +26,7 @@ namespace METS.Classes.Bus.Actions
         private byte _sequence = 0x00;
         private CancellationToken _token;
         private byte _currentSeqNum = 0;
+        private string appId;
 
         public string Type { get; } = "Geräte Info";
         public LineDevice Device { get; set; }
@@ -48,6 +51,10 @@ namespace METS.Classes.Bus.Actions
             if (_state == 1 && response.APCI == Knx.Parser.ApciTypes.PropertyValueResponse)
             {
                 State1(response);
+            }
+            else if(_state == 3 && response.APCI == Knx.Parser.ApciTypes.MemoryResponse)
+            {
+
             }
             else
             {
@@ -91,7 +98,7 @@ namespace METS.Classes.Bus.Actions
         private async void State1(TunnelResponse response)
         {
             ProgressValue = 10;
-            string appId = BitConverter.ToString(response.Data).Replace("-", "").Substring(8);
+            appId = BitConverter.ToString(response.Data).Replace("-", "").Substring(8);
             appId = "M-" + appId.Substring(0, 4) + "_A-" + appId.Substring(4, 4) + "-" + appId.Substring(8, 2) + "-";
 
             //TODO Versionen beachten
@@ -106,6 +113,7 @@ namespace METS.Classes.Bus.Actions
             if(_type == ProgAppType.Minimal && Device.LoadedGroups)
             {
                 State2();
+                return;
             }
 
 
@@ -113,7 +121,6 @@ namespace METS.Classes.Bus.Actions
             CatalogContext context = new CatalogContext();
             ApplicationViewModel appModel = context.Applications.Single(a => a.Id.StartsWith(appId));
             int GroupAddress = 0;
-            int AddressLength = 0;
 
             if (appModel.Table_Group != "" || appModel.Table_Group != null)
             {
@@ -125,6 +132,9 @@ namespace METS.Classes.Bus.Actions
                 //TODO hinzufügen von Adresse aus der Maske holen!
             }
 
+
+            TodoText = "Schreibe Gruppentabelle....";
+            ProgressValue = 20;
 
             //Lenge der Tabelle auf 1 stellen
             _sequence++;
@@ -151,16 +161,10 @@ namespace METS.Classes.Bus.Actions
             //Liste zusammenstelle und sortieren
             List<string> addedGroups = new List<string>();
             foreach (DeviceComObject com in Device.ComObjects)
-            {
                 foreach (GroupAddress group in com.Groups)
-                {
                     if (!addedGroups.Contains(group.GroupName))
-                    {
-                        AddressLength++;
                         addedGroups.Add(group.GroupName);
-                    }
-                }
-            }
+
             addedGroups.Sort();
 
             //Liste zum Datenpaket hinzufügen
@@ -170,7 +174,7 @@ namespace METS.Classes.Bus.Actions
             //Datenlänge richtig setzen
             data[0] = BitConverter.GetBytes(addedGroups.Count * 2)[0];
 
-            builder.Build(UnicastAddress.FromString("0.0.0"), UnicastAddress.FromString(Device.LineName), Knx.Parser.ApciTypes.MemoryWrite, _sequence, 2, data.ToArray());
+            builder.Build(UnicastAddress.FromString("0.0.0"), UnicastAddress.FromString(Device.LineName), Knx.Parser.ApciTypes.MemoryWrite, _sequence, _currentSeqNum++, data.ToArray());
             Connection.Send(builder);
 
             await Task.Delay(1000);
@@ -182,11 +186,15 @@ namespace METS.Classes.Bus.Actions
             Array.Reverse(address);
             data.AddRange(address);
             data.Add(6);
-            builder.Build(UnicastAddress.FromString("0.0.0"), UnicastAddress.FromString(Device.LineName), Knx.Parser.ApciTypes.MemoryWrite, _sequence, 3, data.ToArray());
+            builder.Build(UnicastAddress.FromString("0.0.0"), UnicastAddress.FromString(Device.LineName), Knx.Parser.ApciTypes.MemoryWrite, _sequence, _currentSeqNum++, data.ToArray());
             Connection.Send(builder);
 
 
-            Device.LoadedGroups = true;
+            _ = App._dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Low, () =>
+            {
+                Device.LoadedGroups = true;
+            });
+
 
             State2();
         }
@@ -194,19 +202,40 @@ namespace METS.Classes.Bus.Actions
 
         private async void State2()
         {
+            TodoText = "Schreibe Kommtabelle....";
+            ProgressValue = 30;
+
+
+            //check if table already written
+
+            CatalogContext context = new CatalogContext();
+            ApplicationViewModel appModel = context.Applications.Single(a => a.Id.StartsWith(appId));
+            int ComAddress = 0;
+
+            if (appModel.Table_Group != "" || appModel.Table_Group != null)
+            {
+                AppAbsoluteSegmentViewModel segmentModel = context.AppAbsoluteSegments.Single(s => s.Id == appModel.Table_Object);
+                ComAddress = segmentModel.Address;
+            }
+            else
+            {
+                
+            }
+
+            _sequence++;
+            TunnelRequest builder = new TunnelRequest();
+            List<byte> data = new List<byte> { 12 };
+            byte[] address = BitConverter.GetBytes(Convert.ToInt16(ComAddress + 2));
+            Array.Reverse(address);
+            data.AddRange(address);
+            builder.Build(UnicastAddress.FromString("0.0.0"), UnicastAddress.FromString(Device.LineName), Knx.Parser.ApciTypes.MemoryRead, _sequence, _currentSeqNum++, data.ToArray());
+            Connection.Send(builder);
+            _state = 3;
 
 
 
 
-
-
-
-
-
-            Finish();
-            return;
-
-
+            //Finish();
         }
 
 
@@ -231,6 +260,26 @@ namespace METS.Classes.Bus.Actions
                     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
                 });
             }
+        }
+
+        private async Task<XDocument> GetKnxMaster()
+        {
+            StorageFile masterFile;
+
+            try
+            {
+                masterFile = await ApplicationData.Current.LocalFolder.GetFileAsync("knx_master.xml");
+            }
+            catch
+            {
+                StorageFile defaultFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Data/knx_master.xml"));
+                masterFile = await ApplicationData.Current.LocalFolder.CreateFileAsync("knx_master.xml");
+                await FileIO.WriteTextAsync(masterFile, await FileIO.ReadTextAsync(defaultFile));
+            }
+
+
+            XDocument masterXml = XDocument.Load(await masterFile.OpenStreamForReadAsync());
+            return masterXml;
         }
 
 
