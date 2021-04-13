d70764fd7107cc3a1522a45b0169b7b619dbb107
Mike Gerst <admin@mikegerst.de>
2021-04-13 13:24:44 +0200

    UI fixes

---
+++ Kaenx/Classes/Converter/InterfaceGlyphConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Kaenx.Classes.Converter
+{
+    public class InterfaceGlyphConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var x = value as Type;
+
+            if(x == typeof(Konnect.Interfaces.KnxInterfaceIp)) //IP Interface
+            {
+                return "\xE968"; //EC27
+            } else if(x == typeof(Konnect.Interfaces.KnxInterfaceRemote))
+            {
+                return "\xE774";
+            } else if(x == typeof(Konnect.Interfaces.KnxInterfaceUsb))
+            {
+                return "\xECF0";
+            }/* else if(x == typeof(Konnect.Interfaces.KnxInterfaceIpRouter))
+            {
+                return "\xE704";
+            }*/
+            //Todo add IpRouter
+
+            return "\xF142";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
