4149d0787032860ffe4ebf319f2a1c664253f82f
Mike <45664417+thewhobox@users.noreply.github.com>
2021-07-19 21:52:20 +0200

    Added Import Managers

---
+++ ImportTester/Program.cs
@@ -0,0 +1,16 @@
+﻿using Kaenx.DataContext.Import;
+using Kaenx.DataContext.Import.Manager;
+using System;
+
+namespace ImportTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Getting downloader");
+            IManager manager = ImportManager.GetImportManager(@"C:\Users\mikeg\OneDrive - Mike Gerst\KNX\KNX-Prods\MDT_KP_BE_01_Push_Button_V15a.knxprod");
+        }
+    }
+}
